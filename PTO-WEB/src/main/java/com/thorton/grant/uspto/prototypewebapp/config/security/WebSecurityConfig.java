3b7d3aa5f4dd3ef73da1a3ba794dcea7cd258739
dutchTender <lzhang421@gmail.com>
2018-10-27 16:55:30 -0400

    some what fixed animation issue after added uxpinx svgs.

--- PTO-WEB/src/main/java/com/thorton/grant/uspto/prototypewebapp/config/security/WebSecurityConfig.java
+++ PTO-WEB/src/main/java/com/thorton/grant/uspto/prototypewebapp/config/security/WebSecurityConfig.java
@@ -51,7 +51,7 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().sameOrigin().and()
 
                 .authorizeRequests()
-                .antMatchers( "/resources/**", "/h2-console/**", "/console/**","/webjars/**","/registration/**","/","/home","/newUser","/aboutUs/**","/contact/**","/public/**","/owner/**","account/user/email/update/**").permitAll()
+                .antMatchers( "/resources/**", "/h2-console/**", "/console/**","/webjars/**","/registration/**","/","/home","/newUser","/aboutUs/**","/contact/**","/public/**","/owner/**").permitAll()
                 //.antMatchers("/vets**","/owners**").hasRole("vet")
                 //.antMatchers("/owners","owner","/pet").hasRole("owner")
                 //.antMatchers("/owner**","/vet**","/pet**").hasRole("admin")
