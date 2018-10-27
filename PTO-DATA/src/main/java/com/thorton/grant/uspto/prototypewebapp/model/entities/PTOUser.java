3cef19e00716f01135d715580d81968fe1e31534
dutchTender <lzhang421@gmail.com>
2018-10-26 21:04:41 -0400

    removed patents and added user name algo comment. closes #3.

--- PTO-DATA/src/main/java/com/thorton/grant/uspto/prototypewebapp/model/entities/PTOUser.java
+++ PTO-DATA/src/main/java/com/thorton/grant/uspto/prototypewebapp/model/entities/PTOUser.java
@@ -4,26 +4,19 @@ package com.thorton.grant.uspto.prototypewebapp.model.entities;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
 @Table(name = "PTOUsers")
 public class PTOUser extends  UserPersonalData{
 
-    @OneToMany(mappedBy = "patentOwner")
-    private Set<Patent> userPatents;
 
     @OneToMany(mappedBy = "trademarkOwner")
     private Set<TradeMark> userTrademarks;
 
 
-    public Set<Patent> getUserPatents() {
-        return userPatents;
-    }
 
-    public void setUserPatents(Set<Patent> userPatents) {
-        this.userPatents = userPatents;
-    }
 
     public Set<TradeMark> getUserTrademarks() {
         return userTrademarks;
@@ -31,5 +24,27 @@ public class PTOUser extends  UserPersonalData{
 
     public void setUserTrademarks(Set<TradeMark> userTrademarks) {
         this.userTrademarks = userTrademarks;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        PTOUser ptoUser = (PTOUser) o;
+        return Objects.equals(userTrademarks, ptoUser.userTrademarks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), userTrademarks);
+    }
+
+    @Override
+    public String toString() {
+        return "PTOUser{" +
+                "userTrademarks=" + userTrademarks +
+                '}';
     }
 }
