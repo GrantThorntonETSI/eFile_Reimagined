320e85099079a95a7deb2dcb142ccad386f269d6
dutchTender <lzhang421@gmail.com>
2018-11-03 16:53:54 -0400

    entity design update progress.

--- PTO-DATA/src/main/java/com/thorton/grant/uspto/prototypewebapp/model/entities/USPTO/tradeMark/application/participants/Contact.java
+++ PTO-DATA/src/main/java/com/thorton/grant/uspto/prototypewebapp/model/entities/USPTO/tradeMark/application/participants/Contact.java
@@ -1,7 +1,24 @@
 package com.thorton.grant.uspto.prototypewebapp.model.entities.USPTO.tradeMark.application.participants;
 
 
+import com.thorton.grant.uspto.prototypewebapp.model.entities.USPTO.tradeMark.application.BaseTrademarkApplication;
+import com.thorton.grant.uspto.prototypewebapp.model.entities.USPTO.user.UserPersonalData;
+
+import javax.persistence.ManyToMany;
+import java.util.Date;
+import java.util.Set;
+
+public class Contact  extends UserPersonalData {
+
+    private String companyName;
+    private Date lastContactDate;
+    private String preferredContactMethod;
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+    // tradeMarkApplication is the owning object, Contact is the subordinate object here
+    ////////////////////////////////////////////////////////////////////////////////////////
+    @ManyToMany
+    Set<BaseTrademarkApplication> applications;
 
 
-public class Contact {
 }
