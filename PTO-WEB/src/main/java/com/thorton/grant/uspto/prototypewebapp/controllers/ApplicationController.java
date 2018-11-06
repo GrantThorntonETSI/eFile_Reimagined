99c4f273b1e1f606f725a5d2e39a7d615c923481
dutchTender <lzhang421@gmail.com>
2018-11-05 22:00:00 -0500

    application flow update.

--- PTO-WEB/src/main/java/com/thorton/grant/uspto/prototypewebapp/controllers/ApplicationController.java
+++ PTO-WEB/src/main/java/com/thorton/grant/uspto/prototypewebapp/controllers/ApplicationController.java
@@ -53,4 +53,29 @@ public class ApplicationController {
     }
 
 
+    // hopefully just a redirect here, we won't need to add the applicaiton and credentials to the model
+    @RequestMapping({"/application/continueApplication/application/OwnerStart", "/application/application/OwnerStart"})
+    public String ownerStart(Model model){
+
+        // create a new application and tie it to user then save it to repository
+        // create attorneyDTO + to model
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        System.out.println("logged in user email :"+authentication.getPrincipal());
+        System.out.println("logged in user name: "+authentication.getName());
+        PTOUserService ptoUserService = serviceBeanFactory.getPTOUserService();
+        PTOUser ptoUser = ptoUserService.findByEmail(authentication.getName());
+
+        UserCredentialsService userCredentialsService = serviceBeanFactory.getUserCredentialsService();
+        UserCredentials credentials = userCredentialsService.findByEmail(authentication.getName());
+
+        model.addAttribute("user", ptoUser);
+        model.addAttribute("account",credentials);
+
+
+
+        return "application/OwnerStart";
+    }
+
+
 }
