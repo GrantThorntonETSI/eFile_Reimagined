d2cc61e71bd2d75043fe243e27df677708a36363
dutchTender <lzhang421@gmail.com>
2018-11-13 22:03:24 -0500

    attorney info. completed back end save for removing contact from application using delete icon. still need to implement back end for removing contact using widget.

--- PTO-WEB/src/main/java/com/thorton/grant/uspto/prototypewebapp/service/REST/ApplicationService.java
+++ PTO-WEB/src/main/java/com/thorton/grant/uspto/prototypewebapp/service/REST/ApplicationService.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.transaction.Transactional;
 import java.io.Console;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -311,10 +312,8 @@ public class ApplicationService {
     @CrossOrigin(origins = {"http://localhost:80","http://efile-reimagined.com"})
     @RequestMapping(method = GET, value="/REST/apiGateway/application/contacts/add/{contact_email}/{appInternalID}")
     @ResponseBody
-    ResponseEntity<String> updateApplicationContactsPool(@PathVariable String contact_email , @PathVariable String appInternalID){
+    ResponseEntity<String> updateApplicationAddContactsPool(@PathVariable String contact_email , @PathVariable String appInternalID){
 
-
-        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -483,4 +482,108 @@ public class ApplicationService {
         return ResponseEntity.ok().headers(responseHeader).body(responseMsg) ;
 
     }
+
+
+
+
+    @CrossOrigin(origins = {"http://localhost:80","http://efile-reimagined.com"})
+    @RequestMapping(method = GET, value="/REST/apiGateway/application/contacts/delete/{contact_email}/{appInternalID}")
+    @ResponseBody
+    ResponseEntity<String> updateApplicationRemoveContactsPool(@PathVariable String contact_email , @PathVariable String appInternalID){
+
+
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        //UserCredentialsService userCredentialsService = serviceBeanFactory.getUserCredentialsService();
+        PTOUserService ptoUserService = serviceBeanFactory.getPTOUserService();
+        //UserCredentials userCredentials = userCredentialsService.findByEmail(email);
+        PTOUser ptoUser = ptoUserService.findByEmail(email);// ?? we may not need to save this
+        // verify authentication is valid before moving on ....
+        // have to have a valid session
+
+        if(ptoUser == null || contact_email == ""){
+            ////////////////////////////////////////////////
+            // start generating response
+            ////////////////////////////////////////////////
+            String statusCode = "404";
+            String responseMsg = "Contact with email address :"+contact_email+ " has not been saved. invalid user session.";
+            responseMsg = "{status:" + statusCode +" } { msg:"+responseMsg+" }";
+            HttpHeaders responseHeader = new HttpHeaders ();
+            //responseHeader.setAccessControlAllowOrigin("http://efile-reimagined.com");
+            responseHeader.setAccessControlAllowOrigin(hostBean.getHost()+hostBean.getPort());
+            ArrayList<String> headersAllowed = new ArrayList<String>();
+            headersAllowed.add("Access-Control-Allow-Origin");
+            responseHeader.setAccessControlAllowHeaders(headersAllowed);
+            ArrayList<String> methAllowed = new ArrayList<String>();
+
+            return ResponseEntity.ok().headers(responseHeader).body(responseMsg) ;
+
+        }
+        //////////////////////////////////////////////////////////
+        // retrieve application using passed internal id
+        //////////////////////////////////////////////////////////
+        BaseTradeMarkApplicationService baseTradeMarkApplicationService = serviceBeanFactory.getBaseTradeMarkApplicationService();
+        BaseTrademarkApplication baseTrademarkApplication = baseTradeMarkApplicationService.findByInternalID(appInternalID);
+        //////////////////////////////////////////////////////////
+        // find contact via email from PTOUser
+        // create a copy of the contact object
+        // add the copy of the contact to the application object
+        // save application object
+        ///////////////////////////////////////////////////////////
+
+        // check if lawyer is set as primary ...if so, remove that first
+         Lawyer delLawyer = baseTrademarkApplication.getPrimaryLawyer();
+         if(delLawyer != null){
+             if(delLawyer.getEmail().equals(contact_email)){
+                 baseTrademarkApplication.setPrimaryLawyer(null);
+             }
+
+         } // else primary is not set and nothing to do there
+        // check if contact email is in application lawyer pool ..if so, remove
+        for(Iterator<Lawyer> iter = baseTrademarkApplication.getAvailableLawyers().iterator(); iter.hasNext(); ) {
+            delLawyer = null;
+            delLawyer = iter.next();
+            if(delLawyer.getEmail().equals(contact_email)){
+
+                baseTrademarkApplication.removeAvailableLawyer(delLawyer);
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        ////////////////////////////////////////////////
+        // start generating response
+        ////////////////////////////////////////////////
+        String statusCode = "200";
+        String responseMsg = "Contact with email address :"+contact_email+" has been removed from the application.";
+        responseMsg = "{status:" + statusCode +" } { msg:"+responseMsg+" }";
+        HttpHeaders responseHeader = new HttpHeaders ();
+        //responseHeader.setAccessControlAllowOrigin("http://efile-reimagined.com");
+
+        responseHeader.setAccessControlAllowOrigin(hostBean.getHost()+hostBean.getPort());
+        ArrayList<String> headersAllowed = new ArrayList<String>();
+        headersAllowed.add("Access-Control-Allow-Origin");
+        responseHeader.setAccessControlAllowHeaders(headersAllowed);
+        ArrayList<String> methAllowed = new ArrayList<String>();
+
+        System.out.println("response header : "+responseHeader.getAccessControlAllowOrigin());
+
+
+        return ResponseEntity.ok().headers(responseHeader).body(responseMsg) ;
+
+    }
+
+
+
 }
