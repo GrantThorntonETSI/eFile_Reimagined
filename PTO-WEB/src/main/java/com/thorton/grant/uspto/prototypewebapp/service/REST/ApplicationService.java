6fcac9418113ff466010eafdfdd9de08d3dd7643
dutchTender <lzhang421@gmail.com>
2018-11-23 16:39:14 -0500

    code refactor for Application services. collapse duplicate code. #88.

--- PTO-WEB/src/main/java/com/thorton/grant/uspto/prototypewebapp/service/REST/ApplicationService.java
+++ PTO-WEB/src/main/java/com/thorton/grant/uspto/prototypewebapp/service/REST/ApplicationService.java
@@ -204,15 +204,9 @@ public class ApplicationService {
     @ResponseBody
     ResponseEntity<String> updateApplicationOwner(@PathVariable String applicationField , @PathVariable String param, @PathVariable String appInternalID){
 
-
-         System.out.println("############################################################");
-         System.out.println(applicationField);
-         System.out.println(param);
-         System.out.println(appInternalID);
-         System.out.println("############################################################");
          // verify token before preceding
 
-
+        /*
          Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
          String email = authentication.getName();
          //UserCredentialsService userCredentialsService = serviceBeanFactory.getUserCredentialsService();
@@ -239,7 +233,13 @@ public class ApplicationService {
 
             return ResponseEntity.ok().headers(responseHeader).body(responseMsg) ;
 
+        }*/
+        if(verifyValidUserSession("xxx") == false){
+            return buildResponseEnity(applicationField,"404", "", "has not been saved. invalid user session.");
+
         }
+
+
         // retrieve application using passed internal id
         BaseTradeMarkApplicationService baseTradeMarkApplicationService = serviceBeanFactory.getBaseTradeMarkApplicationService();
         BaseTrademarkApplication baseTrademarkApplication = baseTradeMarkApplicationService.findByInternalID(appInternalID);
