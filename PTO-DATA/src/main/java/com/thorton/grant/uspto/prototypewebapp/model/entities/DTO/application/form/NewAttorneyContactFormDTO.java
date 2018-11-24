240f292d4e16e052fad6ccfa9f22511b7a15e27c
dutchTender <lzhang421@gmail.com>
2018-11-23 20:42:13 -0500

    created form DTO object for add new attorney. #90.

---
+++ PTO-DATA/src/main/java/com/thorton/grant/uspto/prototypewebapp/model/entities/DTO/application/form/NewAttorneyContactFormDTO.java
@@ -0,0 +1,184 @@
+package com.thorton.grant.uspto.prototypewebapp.model.entities.DTO.application.form;
+
+public class NewAttorneyContactFormDTO {
+
+
+    private String title;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String suffix;
+
+    private String lawFirmName;
+
+
+    private String attorneyAddressCountry;
+    private String attorneyAddress1;
+
+    private String attorneyAddress2;
+
+    private String attorneyAddress3;
+
+    private String attorneyCity;
+
+    private String attorneyState;
+
+    private String attorneyZipcode;
+
+    private String attorneyEmail;
+
+
+
+    private String attorneyPhone;
+
+    private String attorneyDocketNumber;
+
+    private String attorneyAffiliation;
+
+    private String appInternalID;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+
+    public String getLawFirmName() {
+        return lawFirmName;
+    }
+
+    public void setLawFirmName(String lawFirmName) {
+        this.lawFirmName = lawFirmName;
+    }
+
+    public String getAttorneyAddressCountry() {
+        return attorneyAddressCountry;
+    }
+
+    public void setAttorneyAddressCountry(String attorneyAddressCountry) {
+        this.attorneyAddressCountry = attorneyAddressCountry;
+    }
+
+    public String getAttorneyAddress1() {
+        return attorneyAddress1;
+    }
+
+    public void setAttorneyAddress1(String attorneyAddress1) {
+        this.attorneyAddress1 = attorneyAddress1;
+    }
+
+    public String getAttorneyAddress2() {
+        return attorneyAddress2;
+    }
+
+    public void setAttorneyAddress2(String attorneyAddress2) {
+        this.attorneyAddress2 = attorneyAddress2;
+    }
+
+    public String getAttorneyAddress3() {
+        return attorneyAddress3;
+    }
+
+    public void setAttorneyAddress3(String attorneyAddress3) {
+        this.attorneyAddress3 = attorneyAddress3;
+    }
+
+    public String getAttorneyCity() {
+        return attorneyCity;
+    }
+
+    public void setAttorneyCity(String attorneyCity) {
+        this.attorneyCity = attorneyCity;
+    }
+
+    public String getAttorneyState() {
+        return attorneyState;
+    }
+
+    public void setAttorneyState(String attorneyState) {
+        this.attorneyState = attorneyState;
+    }
+
+    public String getAttorneyZipcode() {
+        return attorneyZipcode;
+    }
+
+    public void setAttorneyZipcode(String attorneyZipcode) {
+        this.attorneyZipcode = attorneyZipcode;
+    }
+
+    public String getAttorneyEmail() {
+        return attorneyEmail;
+    }
+
+    public void setAttorneyEmail(String attorneyEmail) {
+        this.attorneyEmail = attorneyEmail;
+    }
+
+    public String getAttorneyPhone() {
+        return attorneyPhone;
+    }
+
+    public void setAttorneyPhone(String attorneyPhone) {
+        this.attorneyPhone = attorneyPhone;
+    }
+
+    public String getAttorneyDocketNumber() {
+        return attorneyDocketNumber;
+    }
+
+    public void setAttorneyDocketNumber(String attorneyDocketNumber) {
+        this.attorneyDocketNumber = attorneyDocketNumber;
+    }
+
+    public String getAttorneyAffiliation() {
+        return attorneyAffiliation;
+    }
+
+    public void setAttorneyAffiliation(String attorneyAffiliation) {
+        this.attorneyAffiliation = attorneyAffiliation;
+    }
+
+    public String getAppInternalID() {
+        return appInternalID;
+    }
+
+    public void setAppInternalID(String appInternalID) {
+        this.appInternalID = appInternalID;
+    }
+}
