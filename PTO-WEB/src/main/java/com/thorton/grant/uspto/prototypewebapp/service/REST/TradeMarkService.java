2c13eb0c4b12877a25416bc9d619c427508f2ff6
dutchTender <lzhang421@gmail.com>
2018-12-16 13:42:38 -0500

    created base template for mark text details save REST API. #128.

---
+++ PTO-WEB/src/main/java/com/thorton/grant/uspto/prototypewebapp/service/REST/TradeMarkService.java
@@ -0,0 +1,55 @@
+package com.thorton.grant.uspto.prototypewebapp.service.REST;
+
+import com.thorton.grant.uspto.prototypewebapp.config.host.bean.endPoint.HostBean;
+import com.thorton.grant.uspto.prototypewebapp.factories.ServiceBeanFactory;
+import com.thorton.grant.uspto.prototypewebapp.interfaces.USPTO.tradeMark.application.types.BaseTradeMarkApplicationService;
+import com.thorton.grant.uspto.prototypewebapp.model.entities.USPTO.tradeMark.application.types.BaseTrademarkApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+public class TradeMarkService extends BaseRESTapiService {
+
+    public TradeMarkService(ServiceBeanFactory serviceBeanFactory, HostBean hostBean) {
+        super(serviceBeanFactory, hostBean);
+    }
+
+    @CrossOrigin(origins = {"http://localhost:80","http://efile-reimagined.com"})
+    @RequestMapping(method = GET, value="/REST/apiGateway/mark/update/{markField}/{markValue}/{appInternalID}")
+    @ResponseBody
+    ResponseEntity<String> updateApplicationFields(@PathVariable String markField , @PathVariable String markValue, @PathVariable String appInternalID){
+
+
+        if(verifyValidUserSession("xxx") == false){
+
+            String responseMsg = markField+" has not been saved. invalid user session.";
+            return buildResponseEnity("404", responseMsg );
+
+        }
+
+        // retrieve application using passed internal id
+        //BaseTrademarkApplication baseTrademarkApplication = baseTradeMarkApplicationService.findByInternalID(appInternalID);
+        String appFieldReadable = "";
+        BaseTradeMarkApplicationService baseTradeMarkApplicationService = getServiceBeanFactory().getBaseTradeMarkApplicationService();
+        BaseTrademarkApplication baseTrademarkApplication = baseTradeMarkApplicationService.findByInternalID(appInternalID);
+
+        // page 1 fields
+        if(markField.equals("")){
+            // ptoUser.setState(param); // sets state code
+
+        }
+
+        String responseMsg = appFieldReadable+" has been saved.";
+
+        //return ResponseEntity.ok().headers(responseHeader).body(responseMsg) ;
+        return buildResponseEnity("200", responseMsg);
+    }
+
+
+
+
+}
