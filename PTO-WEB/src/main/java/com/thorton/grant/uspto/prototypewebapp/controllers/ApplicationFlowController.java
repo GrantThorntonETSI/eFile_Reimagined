808dcaa7e16c34e197ad7a436a49ccadd833b04f
lizhang-at-726794120467 <li.zhang@us.gt.com>
2019-01-02 15:45:59 -0500

    completed trust form rework conversion.  #218.

--- PTO-WEB/src/main/java/com/thorton/grant/uspto/prototypewebapp/controllers/ApplicationFlowController.java
+++ PTO-WEB/src/main/java/com/thorton/grant/uspto/prototypewebapp/controllers/ApplicationFlowController.java
@@ -527,10 +527,6 @@ public class ApplicationFlowController {
         newOwnerContactFormDTO.addPartner(partner);
 
 
-
-
-
-
         model.addAttribute("addNewOwnerContactFormDTO", newOwnerContactFormDTO);
 
         return "application/owner/partnership/ownerInfo2";
@@ -560,15 +556,40 @@ public class ApplicationFlowController {
         partnerDTO partner = new partnerDTO();
         newOwnerContactFormDTO.addPartner(partner);
 
+        model.addAttribute("addNewOwnerContactFormDTO", newOwnerContactFormDTO);
+
+        return "application/owner/llp/ownerInfo";
+    }
 
 
+    @RequestMapping({"/application/owner/us/jointVC"})
+    public String ownerJointVCUSInfo(WebRequest request, Model model, @RequestParam("trademarkID") String trademarkInternalID) {
 
+        // create a new application and tie it to user then save it to repository
+        // create attorneyDTO + to model
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PTOUserService ptoUserService = serviceBeanFactory.getPTOUserService();
+        PTOUser ptoUser = ptoUserService.findByEmail(authentication.getName());
+        UserCredentialsService userCredentialsService = serviceBeanFactory.getUserCredentialsService();
+        UserCredentials credentials = userCredentialsService.findByEmail(authentication.getName());
 
+        model.addAttribute("user", ptoUser);
+        model.addAttribute("account",credentials);
+        BaseTradeMarkApplicationService baseTradeMarkApplicationService = serviceBeanFactory.getBaseTradeMarkApplicationService();
+        BaseTrademarkApplication baseTrademarkApplication = baseTradeMarkApplicationService.findByInternalID(trademarkInternalID);
+        model.addAttribute("baseTrademarkApplication", baseTrademarkApplication);
+
+        model.addAttribute("hostBean", hostBean);
+
+        NewOwnerContactFormDTO newOwnerContactFormDTO = new NewOwnerContactFormDTO();
+        partnerDTO partner = new partnerDTO();
+        newOwnerContactFormDTO.addPartner(partner);
 
         model.addAttribute("addNewOwnerContactFormDTO", newOwnerContactFormDTO);
 
-        return "application/owner/partnership/ownerInfo2";
+        return "application/owner/jointVC/ownerInfo";
     }
+
 
 
 
