5b7df9b3b7d7fe203b384e4f71b382f42f49cf3b
dutchTender <lzhang421@gmail.com>
2019-01-10 18:42:54 -0500

    added persistence behavior for js file upload. #264.

--- PTO-DATA/src/main/java/com/thorton/grant/uspto/prototypewebapp/model/entities/USPTO/tradeMark/application/types/BaseTrademarkApplication.java
+++ PTO-DATA/src/main/java/com/thorton/grant/uspto/prototypewebapp/model/entities/USPTO/tradeMark/application/types/BaseTrademarkApplication.java
@@ -548,6 +548,7 @@ public class BaseTrademarkApplication  {
 
                 if(gsClassCategory.getClassCategoryNumber() == Integer.valueOf(current.getClassNumber())){
                     gsClassCategory.addGoodAndService(current);
+                    gsClassCategory.setClassCategoryImagePath(current.getClassSpecimenImgPath());
                 }
             }
             Collections.sort(gsClassCategory.getGoodAndServices(), new CustomComparator());
