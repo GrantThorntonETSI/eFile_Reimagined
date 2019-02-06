2dccc1f1a6fca86640b3f8c03b5fb2f750700c6b
dutchTender <lzhang421@gmail.com>
2019-02-06 14:11:42 -0500

    added base line owner edit html.

--- PTO-WEB/src/main/java/com/thorton/grant/uspto/prototypewebapp/controllers/ApplicationFlowController.java
+++ PTO-WEB/src/main/java/com/thorton/grant/uspto/prototypewebapp/controllers/ApplicationFlowController.java
@@ -484,6 +484,36 @@ public class ApplicationFlowController {
     }
 
 
+    // hopefully just a redirect here, we won't need to add the applicaiton and credentials to the model
+    @RequestMapping({"/application/owner/Ind/edit"})
+    public String ownerIndvUSInfoEdit(WebRequest request, Model model, @RequestParam("trademarkID") String trademarkInternalID) {
+
+        // create a new application and tie it to user then save it to repository
+        // create attorneyDTO + to model
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PTOUserService ptoUserService = serviceBeanFactory.getPTOUserService();
+        PTOUser ptoUser = ptoUserService.findByEmail(authentication.getName());
+        UserCredentialsService userCredentialsService = serviceBeanFactory.getUserCredentialsService();
+        UserCredentials credentials = userCredentialsService.findByEmail(authentication.getName());
+
+        model.addAttribute("user", ptoUser);
+        model.addAttribute("account",credentials);
+        BaseTradeMarkApplicationService baseTradeMarkApplicationService = serviceBeanFactory.getBaseTradeMarkApplicationService();
+        BaseTrademarkApplication baseTrademarkApplication = baseTradeMarkApplicationService.findByInternalID(trademarkInternalID);
+
+
+        model.addAttribute("baseTrademarkApplication", baseTrademarkApplication);
+
+        model.addAttribute("hostBean", hostBean);
+
+
+
+
+        return "application/owner/individual/ownerInfoEdit";
+    }
+
+
+
     @RequestMapping({"/application/owner/us/solp"})
     public String ownerSolPUSInfo(WebRequest request, Model model, @RequestParam("trademarkID") String trademarkInternalID) {
 
