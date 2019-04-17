58451edad8cfb4de5f7eaad4caaa54ba90025fad
lizhang-at-726794120467 <li.zhang@us.gt.com>
2019-04-17 10:11:56 -0500

    integrated lynn's fix for collapsible trigger glyphs. for attorney review. still need to implement for owner review. #195.

--- PTO-WEB/src/main/resources/static/resources/js/lynnjqueryscript.js
+++ PTO-WEB/src/main/resources/static/resources/js/lynnjqueryscript.js
@@ -1241,7 +1241,20 @@ $(document).ready(function(){
 		//$( this ).children('span.glyphicon-chevron-down').toggleClass('visuallyadded');
 		$( this ).children('span.glyphicon-triangle-right').toggleClass('visuallyremoved');
 		$( this ).children('span.glyphicon-triangle-bottom').toggleClass('visuallyadded');
-	});
+	});	//edit owner accordion button toggle rev
+    $( document ).on('click','#editowner button.Accordion-trigger, #reviewattorney button.Accordion-trigger', function() {
+        $( this ).siblings('span.glyphicon-triangle-right').removeClass('visuallyadded').toggleClass('visuallyremoved');
+        $( this ).siblings('span.glyphicon-triangle-bottom').removeClass('visuallyremoved').toggleClass('visuallyadded');
+    });
+    $( document ).on('click','#editowner .panel-title span.glyphicon-triangle-right, #reviewattorney .panel-title span.glyphicon-triangle-right', function() {
+        $( this ).addClass('visuallyremoved').removeClass('visuallyadded');
+        $( this ).siblings( 'span.glyphicon-triangle-bottom' ).addClass('visuallyadded').removeClass('visuallyremoved');
+    });
+    $( document ).on('click','#editowner .panel-title span.glyphicon-triangle-bottom, #reviewattorney .panel-title span.glyphicon-triangle-bottom', function() {
+        $( this ).addClass('visuallyremoved').removeClass('visuallyadded');
+        $( this ).siblings( 'span.glyphicon-triangle-right' ).addClass('visuallyadded').removeClass('visuallyremoved');
+    });
+    //edit owner accordion button toggle rev
 	//edit owner accordion button toggle
 
 	//END additional phone
